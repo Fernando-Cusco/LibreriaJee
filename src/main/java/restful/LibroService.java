ff949a0e976021c85788a7cb470b53bf22d8846e
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-12-28 10:06:18 -0500

    [fix] webservice votos

--- src/main/java/restful/LibroService.java
+++ src/main/java/restful/LibroService.java
@@ -31,7 +31,6 @@ public class LibroService {
 	@Path("/buscar/{id:[0-9][0-9]*}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Libro buscarLibro(@PathParam("id") int id) {
-		System.out.println(libro.buscarLibro(id));
 		return libro.buscarLibro(id);
 	}
 	

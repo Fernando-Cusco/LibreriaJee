829d5f77a8f8ceae014d2237eb66278c0277203d
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-01-31 23:32:08 -0500

    [fix] recibir varias direcciones del usuario

--- src/main/java/modelotmp/UsuarioTMP.java
+++ src/main/java/modelotmp/UsuarioTMP.java
@@ -1,11 +1,20 @@
 package modelotmp;
 
+import java.sql.Date;
+import java.util.List;
+
+import modelo.Direccion;
+
 public class UsuarioTMP {
 
 	private int id;
 	private String correo;
 	private String password;
+	private Date fecha;
+	private String nombres;
+	private String telefono;
 	
+	private List<Direccion> direcciones;
 	
 	
 	public int getId() {
@@ -25,6 +34,35 @@ public class UsuarioTMP {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public List<Direccion> getDirecciones() {
+		return direcciones;
+	}
+	public void setDirecciones(List<Direccion> direcciones) {
+		this.direcciones = direcciones;
+	}
+	@Override
+	public String toString() {
+		return "UsuarioTMP [id=" + id + ", correo=" + correo + ", password=" + password + ", fecha=" + fecha
+				+ ", nombres=" + nombres + ", telefono=" + telefono + ", direcciones=" + direcciones + "]";
 	}
 	
 	
